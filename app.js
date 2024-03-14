fed4a1fe44162e7f69693e68d3260ed5f25c4ec3
Ozioma45 <oziomaegole@gmail.com>
2024-03-14 16:39:14 +0100

    ejs view engine

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const express = require("express");
 //express app
 const app = express();
 
+//Register view engine
+app.set("view engine", "ejs");
+
 //listen for request
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //res.send("<p>Home Page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>About page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/about-us", (req, res) => {
+app.get("/blogs/create", (req, res) => {
   //res.send("<p>About page</p>");
-  res.redirect("/about");
+  res.render("create");
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
