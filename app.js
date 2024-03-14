0f11e05d320f2ef932e731daab87c4e6290e617d
Ozioma45 <oziomaegole@gmail.com>
2024-03-14 17:46:22 +0100

    passing data to the view engine

--- app.js
+++ app.js
@@ -10,21 +10,35 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
   //res.send("<p>Home Page</p>");
-  res.render("index");
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>About page</p>");
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
   //res.send("<p>About page</p>");
-  res.render("create");
+  res.render("create", { title: "Create a New Blog" });
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
