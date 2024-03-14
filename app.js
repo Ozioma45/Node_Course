ed6b892ca0f5230bbecfae7f2090189014ce2b5a
Ozioma45 <oziomaegole@gmail.com>
2024-03-14 14:51:21 +0100

    redirect and 404

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ app.get("/about", (req, res) => {
   //res.send("<p>About page</p>");
   res.sendFile("./views/about.html", { root: __dirname });
 });
+
+app.get("/about-us", (req, res) => {
+  //res.send("<p>About page</p>");
+  res.redirect("/about");
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
