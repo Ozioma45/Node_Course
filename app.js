47c8113dfcb144bc7db344e3a77d4dbb025e8c3e
Ozioma45 <oziomaegole@gmail.com>
2024-03-29 17:41:20 +0100

    settiing the mongoDB environment

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-//express app
+// Express app
 const app = express();
 
-//connect to mongodb
-const dbURL =
-  "mongodb+srv://Ozioma45:Oziblink2846@nodetut.dzkjohw.mongodb.net/?retryWrites=true&w=majority&appName=NodeTut";
-
-//Register view engine
-app.set("view engine", "ejs");
-
-//listen for request
-app.listen(3000);
-
-//middleware and static files
+// Middleware and Static files
 app.use(express.static("public"));
-
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  console.log("In the next middleware");
-  next();
-});
+// Connect to MongoDB
+const dbURL =
+  "mongodb+srv://Ozioma45:Oziblink2846@nodetut.dzkjohw.mongodb.net/nodeTut?retryWrites=true&w=majority&appName=NodeTut";
 
+(async () => {
+  try {
+    await mongoose.connect(dbURL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+})();
+
+// Register view engine
+app.set("view engine", "ejs");
+
+// Routes
 app.get("/", (req, res) => {
   const blogs = [
     {
@@ -39,21 +41,24 @@ app.get("/", (req, res) => {
       snippet: "Lorem ipsum dolor sit amet consectetur",
     },
   ];
-  //res.send("<p>Home Page</p>");
   res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  //res.send("<p>About page</p>");
   res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  //res.send("<p>About page</p>");
   res.render("create", { title: "Create a New Blog" });
 });
 
-//404 page
+// 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
